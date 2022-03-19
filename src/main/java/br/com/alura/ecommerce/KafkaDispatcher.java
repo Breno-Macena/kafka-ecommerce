c3096e2ebefacfbcbd4d9a84481861aea504005d
Breno Macena <br.macena@hotmail.com>
2022-03-19 17:49:02 -0300

    Creating Order class and serialize config with gson

--- src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
+++ src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
@@ -7,12 +7,11 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-public class KafkaDispatcher implements Closeable {
-    private final KafkaProducer<String, String> producer;
+public class KafkaDispatcher<T> implements Closeable {
+    private final KafkaProducer<String, T> producer;
 
     KafkaDispatcher(){
         this.producer = new KafkaProducer<>(properties());
@@ -24,11 +23,11 @@ public class KafkaDispatcher implements Closeable {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         // tanto a chave quanto o valor vão transformar a mensagem e a chave baseada em string (string para byte)
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
         return properties;
     }
 
-    void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
+    void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
