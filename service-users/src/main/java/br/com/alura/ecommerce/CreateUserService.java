665c240aefc01af9ffbf4d0375e68b1136e7d0c3
Breno Macena <br.macena@hotmail.com>
2022-03-26 21:49:10 -0300

    Batching proccess

--- service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
+++ service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
@@ -24,10 +24,10 @@ public class CreateUserService {
     }
 
     public static void main(String[] args) throws SQLException {
-        var fraudDetectorService = new CreateUserService();
+        var createUserService = new CreateUserService();
         try(var service = new KafkaService<>(CreateUserService.class.getSimpleName(),
                 "ECOMMERCE_NEW_ORDER",
-                fraudDetectorService::parse,
+                createUserService::parse,
                 Order.class,
                 Map.of())){
             service.run();
