175829e25793c18bf13cab97de088b271e142aad
Breno Macena <br.macena@hotmail.com>
2022-04-01 11:56:49 -0300

    Service email new order

--- service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
+++ service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -13,13 +13,11 @@ import java.util.concurrent.ExecutionException;
 
 public class NewOrderServlet extends HttpServlet {
     private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
-    private final KafkaDispatcher<Email> emailDispatcher = new KafkaDispatcher<>();
 
     @Override
     public void destroy() {
         super.destroy();
         orderDispatcher.close();
-        emailDispatcher.close();
     }
 
     @Override
@@ -32,9 +30,6 @@ public class NewOrderServlet extends HttpServlet {
 
             var order = new Order(orderId, email, amount);
             orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, new CorrelationId(NewOrderServlet.class.getSimpleName()), order);
-
-            var emailCode = new Email("New Order", "Thank you for your order! We are processing your order!");
-            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, new CorrelationId(NewOrderServlet.class.getSimpleName()),  emailCode);
 
             System.out.println("New order sent successfully.");
             resp.setStatus(HttpServletResponse.SC_OK);
--- service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -9,21 +9,16 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try(var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try(var emailDispatcher = new KafkaDispatcher<Email>()) {
-                var email = Math.random() + "@email.com";
-                for (int i = 0; i < 10; i++) {
-                    var orderId = UUID.randomUUID().toString();
-                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            var email = Math.random() + "@email.com";
+            for (int i = 0; i < 10; i++) {
+                var orderId = UUID.randomUUID().toString();
+                var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
 
-                    var id = new CorrelationId(NewOrderMain.class.getSimpleName());
+                var id = new CorrelationId(NewOrderMain.class.getSimpleName());
 
-                    var order = new Order(orderId, email, amount);
-                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, id, order);
-
-                    var emailCode = new Email("New Order", "Thank you for your order! We are processing your order!");
-                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, id, emailCode);
-                }
+                var order = new Order(orderId, email, amount);
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, id, order);
             }
         }
     }
